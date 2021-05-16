c35f013c281b1d32f966ace8bf37e24cdeabbaf3
Thomas <thomasfindlay94@gmail.com>
2021-05-16 13:46:17 +0100

    features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import { Editor, EditorTools } from "@progress/kendo-react-editor";
+import { loadMessages, LocalizationProvider } from "@progress/kendo-react-intl";
+import { deMessages } from "./deMessages";
 import content from "./content";
 
+loadMessages(deMessages, "de");
+
 const {
+  FindAndReplace,
+  Pdf,
   Bold,
   Italic,
   Underline,
@@ -20,21 +26,35 @@ const {
   Unlink,
 } = EditorTools;
 
+const CustomPdf = props => (
+  <Pdf
+    {...props}
+    savePdfOptions={{
+      fileName: "React Rich Text Editor",
+      paperSize: "A4",
+      marign: "3cm",
+    }}
+  />
+);
+
 function App() {
   return (
-    <div className="app">
-      <Editor
-        tools={[
-          [Bold, Italic, Underline],
-          [Undo, Redo],
-          [Link, Unlink],
-          [AlignLeft, AlignCenter, AlignRight],
-          [OrderedList, UnorderedList, Indent, Outdent],
-        ]}
-        contentStyle={{ height: 320 }}
-        defaultContent={content}
-      />
-    </div>
+    <LocalizationProvider language="de">
+      <div className="app">
+        <Editor
+          tools={[
+            [Bold, Italic, Underline],
+            [Undo, Redo],
+            [Link, Unlink],
+            [AlignLeft, AlignCenter, AlignRight],
+            [OrderedList, UnorderedList, Indent, Outdent],
+            [FindAndReplace, CustomPdf],
+          ]}
+          contentStyle={{ height: 320 }}
+          defaultContent={content}
+        />
+      </div>
+    </LocalizationProvider>
   );
 }
 
